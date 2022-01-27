0c628a69e1b85fa0243ee6c7821c7ad42c5a91a1
lisontowind <47842345+lisontowind@users.noreply.github.com>
2022-01-27 16:07:55 +0800

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,108 +1,47 @@
 function addRoundTab(){
 	//把div追加到body
-    var x = document.getElementsByClassName("dock__item")
-    var i;
-    for (i = 0; i < x.length; i++) {
-
-        //创建一个div
-        var div = document.createElement("div");        
-        //为div创建属性class = "test"
-        var divattr = document.createAttribute("class");
-        divattr.value = "RoundTab"; 
-        //把属性class = "test"添加到div
-        div.setAttributeNode(divattr);
-        var style = document.createAttribute("style");
-        div.setAttributeNode(style);
-        // div.style.left=(x[i].offsetLeft+"px")
-        // div.style.top =(x[i].offsetTop+"px")
-        // div.style.width = (x[i].offsetWidth+"px")
-        // div.style.height = (x[i].offsetHeight+"px")
-        div.style.width = (10+"px")
-        div.style.height = (10+"px")
-        // x[i].parentNode.appendChild(div)
-        x[i].appendChild(div) 
+    if (document.getElementsByClassName("RoundTab").length < 1){
+        var x = document.getElementsByClassName("dock__item")
+        var i;
+        for (i = 0; i < x.length; i++) {
+            //创建一个div
+            var div = document.createElement("div");        
+            //为div创建属性class = "test"
+            var divattr = document.createAttribute("class");
+            divattr.value = "RoundTab"; 
+            //把属性class = "test"添加到div
+            div.setAttributeNode(divattr);
+            var style = document.createAttribute("style");
+            div.setAttributeNode(style);
+            div.style.float="left";
+            div.style.position="absolute";
+            div.style.width = (0+"px");
+            div.style.height = (0+"px");
+            x[i].appendChild(div) ;
+        }
     }
 }
 function addRoundTab2(){
-	//把div追加到body
-    var x = document.getElementsByClassName("dock__item")
-    var i;
-    for (i = 0; i < x.length; i++) {
-
-        //创建一个div
-        var div = document.createElement("div");        
-        //为div创建属性class = "test"
-        var divattr = document.createAttribute("class");
-        divattr.value = "RoundTab2"; 
-        //把属性class = "test"添加到div
-        div.setAttributeNode(divattr);
-        var style = document.createAttribute("style");
-        div.setAttributeNode(style);
-        // div.style.left=(x[i].offsetLeft+"px")
-        // div.style.top =(x[i].offsetTop+"px")
-        // div.style.width = (x[i].offsetWidth+"px")
-        // div.style.height = (x[i].offsetHeight+"px")
-        div.style.width = (10+"px")
-        div.style.height = (10+"px")
-        // x[i].parentNode.appendChild(div)
-        x[i].appendChild(div) 
-    }
-}
-function addRoundTab3(){
-	//把div追加到body
-    var x = document.getElementsByClassName("dock__item")
-    var i;
-    for (i = 0; i < x.length; i++) {
-
-        //创建一个div
-        var div = document.createElement("div");        
-        //为div创建属性class = "test"
-        var divattr = document.createAttribute("class");
-        divattr.value = "RoundTab3"; 
-        //把属性class = "test"添加到div
-        div.setAttributeNode(divattr);
-        var style = document.createAttribute("style");
-        div.setAttributeNode(style);
-        // div.style.left=(x[i].offsetLeft+"px")
-        // div.style.top =(x[i].offsetTop+"px")
-        // div.style.width = (x[i].offsetWidth+"px")
-        // div.style.height = (x[i].offsetHeight+"px")
-        div.style.width = (10+"px")
-        div.style.height = (10+"px")
-        // x[i].parentNode.appendChild(div)
-        x[i].appendChild(div) 
-    }
-}
-
-function addRoundTab4(){
-	//把div追加到body
-    var x = document.getElementsByClassName("dock__item")
-    var i;
-    for (i = 0; i < x.length; i++) {
-
-        //创建一个div
-        var div = document.createElement("div");        
-        //为div创建属性class = "test"
-        var divattr = document.createAttribute("class");
-        divattr.value = "RoundTab4"; 
-        //把属性class = "test"添加到div
-        div.setAttributeNode(divattr);
-        var style = document.createAttribute("style");
-        div.setAttributeNode(style);
-        // div.style.left=(x[i].offsetLeft+"px")
-        // div.style.top =(x[i].offsetTop+"px")
-        // div.style.width = (x[i].offsetWidth+"px")
-        // div.style.height = (x[i].offsetHeight+"px")
-        div.style.width = (10+"px")
-        div.style.height = (10+"px")
-        // x[i].parentNode.appendChild(div)
-        x[i].appendChild(div) 
+    if (document.getElementsByClassName("RoundTab2").length < 1){
+        var x = document.getElementsByClassName("dock__item")
+        var i;
+        for (i = 0; i < x.length; i++) {
+            var div = document.createElement("div");        
+            var divattr = document.createAttribute("class");
+            divattr.value = "RoundTab2"; 
+            div.setAttributeNode(divattr);
+            var style = document.createAttribute("style");
+            div.setAttributeNode(style);
+            div.style.float="left";
+            div.style.position="absolute";
+            div.style.width = (0+"px");
+            div.style.height = (0+"px");
+            x[i].appendChild(div) ;
+        }
     }
 }
 
 (function(){
-    addRoundTab()
-    addRoundTab2()
-    addRoundTab3()
-    addRoundTab4()
+    addRoundTab();
+    addRoundTab2();
 })()
